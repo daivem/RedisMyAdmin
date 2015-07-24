a15ba7ae20fa315ba7b308775ad40a86b8a64e0d
daivem <daivem@gmail.com>
2015-07-24 23:58:39 +0800

    修正多次刷新时goto不正确的问题

--- application/views/login.php
+++ application/views/login.php
@@ -1,4 +1,20 @@
-<?php PagerWidget::header(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php if (is_ie()){ ?>
+<meta http-equiv=X-UA-Compatible content="IE=edge,chrome=1">
+<?php } ?>
+<title><?= $title; ?></title>
+<link rel="stylesheet" href="<?= base_url('static/css/common.css'); ?>" media="all">
+<link rel="stylesheet" href="<?= base_url('static/css/frame.css'); ?>" media="all">
+<link rel="stylesheet" href="<?= base_url('static/css/index.css'); ?>" media="all">
+<link rel="shortcut icon" href="<?= base_url('static/images/favicon.png'); ?>">
+<script src="<?= base_url('static/js/jquery-1.7.2.min.js'); ?>" type="text/javascript"></script>
+<script src="<?= base_url('static/js/index.js'); ?>" type="text/javascript"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
+
+<body>
 <div style="margin:0 auto; width:960px">
 	<div style="width:300px; margin:0 auto; padding-top:50px;">
 		<form method="POST" action="<?= manager_site_url('login', 'index'); ?>">
