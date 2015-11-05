7d7d3692fa12391f83e966230ecc31eaecdfc857
daivem <daivem@gmail.com>
2015-11-05 21:03:29 +0800

    修正JS的错误

--- application/views/widget/header.php
+++ application/views/widget/header.php
@@ -9,6 +9,10 @@
 <link rel="stylesheet" href="<?= base_url('static/css/frame.css'); ?>" media="all">
 <link rel="stylesheet" href="<?= base_url('static/css/index.css'); ?>" media="all">
 <link rel="shortcut icon" href="<?= base_url('static/images/favicon.png'); ?>">
+<script type="text/javascript">
+var _global = _global || {};
+_global.isLock = false;
+</script>
 <script src="<?= base_url('static/js/jquery-1.7.2.min.js'); ?>" type="text/javascript"></script>
 <script src="<?= base_url('static/js/index.js'); ?>" type="text/javascript"></script>
 <script src="<?= base_url('static/js/frame.js'); ?>" type="text/javascript"></script>
